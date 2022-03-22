bfe1e1f87b5180774875be53812a973b6fb6985c
gustavomazzuco <gustavo_mazzuco@hotmail.com>
2022-03-21 21:22:31 -0300

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,29 +1,53 @@
 import "./App.css";
+import React, { useState } from "react";
+
+function ListaItens({itens = []}){
+  return(
+    <div className="lista-compras-container">
+      <ul className="lista-items">
+        {itens.length === 0 ? (
+        <li className="empty">Nenhum item adicionado</li>
+        ) : (
+        itens.map((item) => (
+          <li>{item}</li>
+        )))}
+      </ul>
+    </div>
+  );
+}
+
+function Formulario() {
+  const [nome, setNome] = useState("");
+  
+  return (
+    <form className="form-add-item" onSubmit={ (event) => {adicionarItem(nome);}} action="" method="post"  >
+      <fieldset>
+        <div className="form-group mb-3">
+          <label htmlFor="item">Adicionar Novo Item na Lista:</label>
+          <input type="text" className="form-control" id="nome" value={nome} onChange={(event) => setNome(event.target.value)} />
+        </div>
+        <button type="submit" className="btn btn-primary">
+          Adicionar
+        </button>
+      </fieldset>
+    </form>
+  );
+}
 
 function App() {
+  const [itens, setItens] = useState([]);
+
+  function adicionarItem(nome){
+    setItens([...itens, nome])
+  }
+
   return (
     <div className="App">
       <header>
         <h2>Lista de Compras:</h2>
-      </header>
-      <div className="lista-compras-container">
-        <ul className="lista-items">
-          <li>Queijo</li>
-          <li>Leite</li>
-          <li>Pão</li>
-        </ul>
-      </div>
-      <form className="form-add-item" action="#" method="post">
-        <fieldset>
-          <div className="form-group mb-3">
-            <label htmlFor="item">Adicionar Novo Item na Lista:</label>
-            <input type="text" className="form-control" id="item" />
-          </div>
-          <button type="submit" className="btn btn-primary">
-            Adicionar
-          </button>
-        </fieldset>
-      </form>
+      </header>      
+    <ListaItens itens={itens}/>
+    <Formulario />
     </div>
   );
 }
