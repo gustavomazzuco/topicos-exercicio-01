2a340425342db935ec92769658e43f47d3a70174
gustavomazzuco <gustavo_mazzuco@hotmail.com>
2022-03-21 22:15:01 -0300

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,30 +1,20 @@
 import "./App.css";
-import React, { useState } from "react";
+import { useState } from "react";
 
-function ListaItens({itens = []}){
-  return(
-    <div className="lista-compras-container">
-      <ul className="lista-items">
-        {itens.length === 0 ? (
-        <li className="empty">Nenhum item adicionado</li>
-        ) : (
-        itens.map((item) => (
-          <li>{item}</li>
-        )))}
-      </ul>
-    </div>
-  );
-}
-
-function Formulario() {
+function Formulario({ adicionarItem }) {
   const [nome, setNome] = useState("");
+  const [quantidade, setQuantidade] = useState("1");
   
   return (
-    <form className="form-add-item" onSubmit={ (event) => {adicionarItem(nome);}} action="" method="post"  >
+    <form className="form-add-item" onSubmit={ (event) => {event.preventDefault(); adicionarItem(nome, quantidade); setNome(""); setQuantidade("1")}} action="" method="post" >
       <fieldset>
         <div className="form-group mb-3">
           <label htmlFor="item">Adicionar Novo Item na Lista:</label>
-          <input type="text" className="form-control" id="nome" value={nome} onChange={(event) => setNome(event.target.value)} />
+          <input type="text" className="form-control" id="produto" value={nome} onChange={(event) => setNome(event.target.value)} required/>
+        </div>
+        <div className="form-group mb-3">
+          <label htmlFor="item">Quantidade:</label>
+          <input type="number" className="form-control" id="qtde" value={quantidade} onChange={(event) => setQuantidade(event.target.value)} required />
         </div>
         <button type="submit" className="btn btn-primary">
           Adicionar
@@ -34,11 +24,27 @@ function Formulario() {
   );
 }
 
+function ListaItens({itens = []}){
+  return(
+    <div className="lista-compras-container">
+      <ul className="lista-items">
+        {itens.length === 0 ? (
+        <li className="empty">Nenhum item adicionado</li>
+        ) : (
+        itens.map((produto) => (
+          <li>{produto.nome} - {produto.quantidade}</li>
+        )))}
+      </ul>
+    </div>
+  );
+}
+
 function App() {
   const [itens, setItens] = useState([]);
 
-  function adicionarItem(nome){
-    setItens([...itens, nome])
+  function adicionarItem(nome, quantidade){
+    setItens([...itens, {nome, quantidade}]);
+    console.log(itens);
   }
 
   return (
@@ -46,8 +52,8 @@ function App() {
       <header>
         <h2>Lista de Compras:</h2>
       </header>      
-    <ListaItens itens={itens}/>
-    <Formulario />
+    <ListaItens itens={itens} />
+    <Formulario adicionarItem={adicionarItem} />
     </div>
   );
 }
